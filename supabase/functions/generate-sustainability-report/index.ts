1b25baec87857dd35daf13d600bef15def941c15
AvallenSolutions <tim@avallen.solutions>
2026-01-10 08:15:48 +0000

    Updated index.ts

--- supabase/functions/generate-sustainability-report/index.ts
+++ supabase/functions/generate-sustainability-report/index.ts
@@ -6,6 +6,9 @@ const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Client-Info, Apikey",
+  "Cache-Control": "no-cache, no-store, must-revalidate",
+  "Pragma": "no-cache",
+  "Expires": "0",
 };
 
 // Skywork tool mapping
